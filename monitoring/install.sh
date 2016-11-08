6e3667fd35a0709538be178899a9fcf42ffff343
jlopezgu <juan.pablo.lopez.gutierrez@intel.com>
2016-11-08 15:10:17 -0600

    testing in an aio server

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -3,5 +3,4 @@ git clone https://github.com/osic/reliability-openstack-ansible-ops.git /opt/rel
 
 # Run the monitoring install script
 cd /opt/reliability-openstack-ansible-ops/cluster_metrics
-chmod +x install.sh
 ./install.sh 

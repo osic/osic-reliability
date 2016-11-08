9b02cebb107c4c6e367ed0951df794fcd7839ef3
jlopezgu <juan.pablo.lopez.gutierrez@intel.com>
2016-11-08 16:10:23 -0600

    Fix path

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,5 +1,5 @@
 # Clone monitoring stack repository into /opt
-git clone https://github.com/osic/reliability-openstack-ansible-ops.git /opt/reliability-openstack-ansible-ops
+git clone https://github.com/osic/reliability-openstack-ansible-ops.git /opt/openstack-ansible-ops
 
 # Run the monitoring install script
 cd /opt/reliability-openstack-ansible-ops/cluster_metrics

10eb284ab106327165ca2b9c70af10029081c79b
jlopezgu <juan.pablo.lopez.gutierrez@intel.com>
2016-11-10 11:45:31 -0600

    deleted reliability

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -2,5 +2,4 @@
 git clone https://github.com/osic/reliability-openstack-ansible-ops.git /opt/openstack-ansible-ops
 
 # Run the monitoring install script
-cd /opt/reliability-openstack-ansible-ops/cluster_metrics
-./install.sh
+bash /opt/openstack-ansible-ops/cluster_metrics/install.sh
